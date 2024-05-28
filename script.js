20575ed49041fdf9ce6272fef83b20e97a94cc3d
ankote <ankote.ayoube.any@gmail.com>
2024-05-28 11:30:40 +0100

    game works with two

--- script.js
+++ script.js
@@ -34,20 +34,40 @@ class Ball {
   update(player1, player2)
   {
 
-        if ((this.x) >= width) {
+        if ((this.x) >= width - player2.playerW - 1) {
 			if ((this.y >= player2.y - player2.playerH / 2) && (this.y < player2.y + player2.playerH / 2) )
 			{
 				this.velX = -(this.velX);
 			}
-            this.velY = -(this.velY);
-            // this.x = width / 2;
-            // this.y = height / 2;
+			else
+			{
+				this.x = width / 2;
+				this.y = height / 2;
+				this.velX = -(this.velX);
+
+				player1.score ++;
+				console.log("player 2 : " + player2.score);
+				console.log("player 1 : " + player1.score);
+
+			}
         }
-        if ((this.x) <= 0) {
-            this.velX = -(this.velX);
-            this.velY = -(this.velY);
-            this.x = width / 2;
-            this.y = height / 2;
+        if ((this.x) <= player1.playerW - 1)
+		{
+		
+			if ((this.y >= player1.y - player1.playerH / 2) && (this.y < player1.y + player1.playerH / 2) )
+			{
+				this.velX = -(this.velX);
+			}
+			else
+			{
+				this.x = width / 2;
+				this.y = height / 2;
+				this.velX = -(this.velX);
+				player2.score ++;
+				console.log("player 2 : " + player2.score);
+				console.log("player 1 : " + player1.score);
+	
+			}
         }
         if ((this.y + this.size) >= height) {
             this.velY = -(this.velY);
@@ -57,18 +77,19 @@ class Ball {
         }
         this.x += this.velX;
         this.y += this.velY;
-  }    
+	}    
 }
 
 class player
 {
-    constructor(x, y, velY, playerH, playerW)
+    constructor(x, y, velY, playerH, playerW, score)
     {
         this.playerH = playerH;
         this.playerW = playerW;
         this.velY = velY;
         this.x = x;
         this.y = y;
+		this.score = score;
     }
     draw()
     {
@@ -121,8 +142,8 @@ function  update(player1, player2)
 
     const size = 5;
     const ball = new Ball(width / 2, height / 2, 3, 3, randomRGB(),size,);
-    const player1 = new player(0, height / 2, 3, 100, 10);
-    const player2 = new player(width - 10, height / 2, 2, 100, 10);
+    const player1 = new player(0, height / 2, 3, 100, 10, 0);
+    const player2 = new player(width - 10, height / 2, 2, 100, 10, 0);
 
 function loop() {
 	ctx.fillStyle = "rgb(152 246 243 / 50%)";
@@ -138,12 +159,12 @@ function loop() {
 	requestAnimationFrame(loop);
 }
 
-loop();
+loop(); 
 
-// document.addEventListener("keydown", event =>
-// { 
-// 	console.log(event.key);
-// });
+document.addEventListener("keydown", event =>
+{ 
+	console.log("ankote reda");
+});
 
 // document.addEventListener("keyup", event =>
 // {
